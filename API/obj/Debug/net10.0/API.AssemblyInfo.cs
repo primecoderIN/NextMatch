77a5e815298cc9ee95a67ca90af1bbad7d782246
Sanjeev <sanjeev2856@gmail.com>
2026-01-17 18:17:53 +0530

    Added gitignore file

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24485a44f478436add88f14077dd9988f665ac4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271c483c7151631d62ac6d3c4aff063eb643e9ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
