4262fc50e66b5949601f090d692e68e3be247eb2
Sanjeev <sanjeev2856@gmail.com>
2026-01-18 12:11:35 +0530

    Made a basic http call using signals and observables

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
+  private http: HttpClient = inject(HttpClient);
   protected readonly title = signal('client');
+  protected members = signal<any>([]);
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+
+  getMembers = async () => {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      throw error;
+    }
+  };
 }
+
+// lastValueFrom(): Returns a promise that resolves with the last value emitted by the observable
+//Similar to .toPromise() but more explicit
+
+// Subscribes internally
+
+// Resolves once
+
+// Automatically completes
+
+// This avoids memory leaks without needing:
+
+// unsubscribe()
+
+// takeUntilDestroyed()
+
+//This approach is preferred for one-time HTTP calls using signals
