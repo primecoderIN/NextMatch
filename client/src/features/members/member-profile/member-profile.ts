c967350dd32daafbe0876db8a022b05728a79761
Sanjeev <sanjeev2856@gmail.com>
2026-01-28 21:38:03 +0530

    Fixed the view profile view issue

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EditableMember, Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
@@ -11,16 +11,16 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css',
 })
-export class MemberProfile implements OnInit,OnDestroy {
+export class MemberProfile implements OnInit, OnDestroy {
   @ViewChild('memberEditForm') memberEditForm: any;
   protected memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   protected member = signal<Member | undefined>(undefined);
-
+  private destroyRef = inject(DestroyRef);
   protected memberEditData?: EditableMember;
 
   ngOnInit(): void {
-    this.route.parent?.data.pipe(takeUntilDestroyed()).subscribe({
+    this.route.parent?.data.pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
       next: (data) => this.member.set(data['member']),
     });
 
@@ -35,14 +35,13 @@ export class MemberProfile implements OnInit,OnDestroy {
   }
 
   ngOnDestroy(): void {
-    if(this.memberService.isEditModeEnabled()){
+    if (this.memberService.isEditModeEnabled()) {
       this.memberService.isEditModeEnabled.set(false);
     }
   }
 
   updateMemberProfile(): void {
-
-    if(!this.memberEditData) return;
+    if (!this.memberEditData) return;
 
     const updatedMember = {
       ...this.member(),
