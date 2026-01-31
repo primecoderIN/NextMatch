b7cf2898f087af5147dcc793314ae720243a7449
Sanjeev <sanjeev2856@gmail.com>
2026-01-31 12:56:39 +0530

    Created member update controller

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -3,20 +3,22 @@ using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using API.DTOs;
+using System.Security.Claims;
 
 
 
 namespace API.Controllers
 {
-     [Authorize] //All endpoints in this controller require authentication unless overridden at method level
+    [Authorize] //All endpoints in this controller require authentication unless overridden at method level
     public class MembersController(IMemberRepository memberRepository) : BaseController
-    {   
+    {
         // [Authorize] //This is optional here as we have added at class level
         // [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers()
         {
-           return Ok(await memberRepository.GetMembersAsync()); 
+            return Ok(await memberRepository.GetMembersAsync());
         }
 
 
@@ -38,9 +40,36 @@ namespace API.Controllers
         public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos([FromRoute] string id)
         {
             return Ok(await memberRepository.GetPhotoForMemberAsync(id));
-            
+
+        }
+
+
+        [HttpPut]  //localhost:5001/api/member
+        public async Task<ActionResult> UpdateMember([FromBody] MemberUpdateDTO memberUpdateDTO)
+        {
+            var memberId = User.FindFirstValue(ClaimTypes.NameIdentifier); //“Give me the logged-in user’s unique ID from the JWT token.”
+
+            if (memberId == null) return BadRequest("Invalid member id");
+            var member = await memberRepository.GetMemberByIdAsyc(memberId);
+
+            if (member == null) return NotFound();
+
+            member.UserName = memberUpdateDTO.UserName ?? member.UserName;
+            member.Description = memberUpdateDTO.Description ?? member.Description;
+            member.City = memberUpdateDTO.City ?? member.City;
+            member.Country = memberUpdateDTO.Country ?? member.Country;
+
+            memberRepository.Update(member); //Marks the entity as modified in the context
+            if (await memberRepository.SaveAllAsync())
+            {
+                return NoContent(); //204 status code
+            }
+
+            return BadRequest("Failed to update member");
         }
     }
+
+
 }
 
 
---
+++ API/DTOs/MemberUpdateDTO.cs
@@ -0,0 +1,10 @@
+
+namespace API.DTOs;
+
+public class MemberUpdateDTO
+{
+    public string? UserName { get; set; }
+    public string? Description { get; set; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
+}
