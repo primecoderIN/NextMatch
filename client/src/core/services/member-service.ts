4cf23e6e611b174eeba0380abe942ef18a33083a
Sanjeev <sanjeev2856@gmail.com>
2026-01-31 22:54:56 +0530

    Completed member update functionality in the client as well

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Member, Photo } from '../../types/member';
+import { EditableMember, Member, Photo } from '../../types/member';
 import { Observable } from 'rxjs';
+import id from '@angular/common/locales/extra/id';
 // import { AccountService } from './account-service';
 
 @Injectable({
@@ -24,6 +25,10 @@ export class MemberService {
 
   getMemberPhotos(id: string): Observable<Photo[]> {
     return this.http.get<Photo[]>(this.baseUrl + `members/${id}` + '/photos');
+  }
+
+  updateMember(member: EditableMember): Observable<void> {
+    return this.http.put<void>(this.baseUrl + `members`, member);
   }
 
   //This now we are doing using interceptor for each request
--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,10 +1,20 @@
-import { Component, DestroyRef, HostListener, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  HostListener,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EditableMember, Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
 import { MemberService } from '../../../core/services/member-service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule, NgForm } from '@angular/forms';
+import { ToastService } from '../../../core/services/toast-service';
 
 @Component({
   selector: 'app-member-profile',
@@ -13,14 +23,15 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './member-profile.css',
 })
 export class MemberProfile implements OnInit, OnDestroy {
-  @ViewChild('editMemberForm') memberEditForm!: NgForm ;
+  @ViewChild('editMemberForm') memberEditForm!: NgForm;
   @HostListener('window:beforeunload', ['$event']) unloadNotification($event: BeforeUnloadEvent) {
     //this will trigger when user tries to close the tab or browser window
     //for in page navigation, we use canDeactivate guard
     if (this.memberEditForm?.dirty) {
       $event.preventDefault();
     }
-  };
+  }
+  protected toast = inject(ToastService);
   protected memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   protected member = signal<Member | undefined>(undefined);
@@ -32,14 +43,12 @@ export class MemberProfile implements OnInit, OnDestroy {
     country: '',
   };
 
-
-
   ngOnInit(): void {
     this.route.parent?.data.pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
       next: (data) => this.member.set(data['member']),
     });
 
-      const member = this.member();
+    const member = this.member();
 
     this.memberEditData = {
       userName: member?.userName || '',
@@ -64,7 +73,16 @@ export class MemberProfile implements OnInit, OnDestroy {
     } as Member;
     // this.member.set(updatedMember);
 
-    console.log('Updated Member Profile:', updatedMember);
-    this.memberService.isEditModeEnabled.set(false);
+    this.memberService
+      .updateMember(this.memberEditData)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: () => {
+          this.toast.success('Profile updated successfully');
+          this.member.set(updatedMember);
+          this.memberEditForm?.reset(updatedMember); //Reset form state after successful update
+          this.memberService.isEditModeEnabled.set(false);
+        },
+      });
   }
 }
