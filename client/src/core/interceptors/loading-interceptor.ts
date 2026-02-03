0a555c76e5ed74ea5c984c6ccb63bd24ab87a09c
Sanjeev <sanjeev2856@gmail.com>
2026-02-03 19:58:19 +0530

    Implemented basic in memory caching is member detail page to improve performance

--- client/src/core/interceptors/loading-interceptor.ts
+++ client/src/core/interceptors/loading-interceptor.ts
@@ -2,14 +2,27 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { BusyService } from '../services/busy-service';
 import { inject } from '@angular/core/primitives/di';
 import { finalize } from 'rxjs/internal/operators/finalize';
-import { delay } from 'rxjs';
+import { delay, of, tap } from 'rxjs';
+
+const cache = new Map<string, any>();
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
-
   const busyService = inject(BusyService);
+
+  if (req.method === 'GET') {
+    const cachedResponse = cache.get(req.url);
+
+    if (cachedResponse) {
+      return of(cachedResponse);
+    }
+  }
+
   busyService.busy();
   return next(req).pipe(
     delay(500), //to avoid flickering for fast requests
-    finalize(() => busyService.idle()) //runs once nomatter success or error
+    tap(response=> {
+        cache.set(req.url, response); //store the response in cache
+    }),
+    finalize(() => busyService.idle()), //runs once nomatter success or error
   );
 };
