f44915770183c09feb0d541dac11d477ea25f7ce
Sanjeev <sanjeev2856@gmail.com>
2026-02-08 10:37:09 +0530

    Migrated to signals to load members

--- client/src/features/members/member-list/member-list.ts
+++ client/src/features/members/member-list/member-list.ts
@@ -1,31 +1,38 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
-import { Observable } from 'rxjs';
 import { Member } from '../../../types/member';
-import { CommonModule } from '@angular/common';
 import { MemberCard } from '../member-card/member-card';
 import { PaginatedResult } from '../../../types/pagination';
 import { Paginator } from '../../../shared/paginator/paginator';
 
 @Component({
   selector: 'app-member-list',
-  imports: [CommonModule, MemberCard, Paginator],
+  imports: [MemberCard, Paginator],
   templateUrl: './member-list.html',
   styleUrl: './member-list.css',
 })
-export class MemberList {
+export class MemberList implements OnInit {
   private memberService = inject(MemberService);
-  protected paginatedMembers$?: Observable<PaginatedResult<Member[]>>;
+  protected paginatedMembers = signal<PaginatedResult<Member[]> | null>(null);
   pageNumber = 1;
   pageSize = 10;
 
-  constructor() {
-    this.paginatedMembers$ = this.memberService.getMembers();
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers(pageNumber?: number, pageSize?: number) {
+    this.memberService.getMembers(pageNumber, pageSize).subscribe({
+      next: (response) => {
+        this.paginatedMembers?.set(response);
+      },
+    });
   }
 
   onPageChange(event: { pageNumber: number; pageSize: number }) {
     this.pageNumber = event.pageNumber;
     this.pageSize = event.pageSize;
-    this.paginatedMembers$ = this.memberService.getMembers(event.pageNumber, event.pageSize);
+
+    this.loadMembers(event.pageNumber, event.pageSize);
   }
 }
