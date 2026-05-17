8bfb4ff9406283bcc8929be1f531ca26200dd4e3
Sanjeev Kumar <sanjeev2856@gmail.com>
2026-05-17 21:40:53 +0530

    Added more comments

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -17,7 +17,7 @@ public string? PublicId {get;set;}
 [JsonIgnore]
 public Member Member {get;set;} = null!;
 
-public string MemberId {get;set;} = null!;
+public string MemberId {get;set;} = null!; //Foreign key to associate photo with a member, this is required because we have a one-to-many relationship between Member and Photo. Each photo belongs to one member, and each member can have multiple photos. By including the MemberId property, we can establish this relationship in the database and ensure that each photo is correctly linked to its corresponding member.
 }
 
 
